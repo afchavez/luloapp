8ad30e3f39ac4c663fde072f283014e47562e6d3
Andres chavez <afchavez40@gmail.com>
2013-10-09 11:25:18 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luloapp::Application.config.secret_token = '72dc9aa7d537ee12e404aff5f5617cb57f9fea0c407bc289ce97a43390f8394a963633a37a78475655b767e450fcec94ecd5844b31de8129ec961fba8004bc97'
